84d491bed1ecfee15c41d24742d030308febddef
Andy <azogg65@bluewin.ch>
2026-01-23 17:39:42 +0100

    Fix: Correct macro name from SOUND_DECODER to SOUND_FEATURE in DecoderCoreConfig.h

--- src/DecoderCoreConfig.h
+++ src/DecoderCoreConfig.h
@@ -10,7 +10,7 @@
 #endif
 
 // Define which type of decoder this is by setting exactly one of the following to 1
-#define SOUND_DECODER 0 // 0 = Sound decoder disabled, 1 = Sound decoder enabled
+#define SOUND_FEATURE 0 // 0 = Sound decoder disabled, 1 = Sound decoder enabled
 #define CAR_DECODER 1   // 0 = Car decoder disabled, 1 = Car decoder enabled
 #define FUNC_DECODER 0  // 0 = Function decoder disabled, 1 = Function decoder enabled
 
