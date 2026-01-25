1b0062216fa5ac8de48c5c837f44c512196da12c
Andy <azogg65@bluewin.ch>
2026-01-25 16:23:11 +0100

    Remove unused Arduino includes and debug message definitions from DecoderCoreConfig.h

--- src/DecoderCoreConfig.h
+++ src/DecoderCoreConfig.h
@@ -3,12 +3,6 @@
 #ifndef _DecoderCoreConfig_h
 #define _DecoderCoreConfig_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
-#else
-	#include "WProgram.h"
-#endif
-
 // Define which type of decoder this is by setting exactly one of the following to 1
 #define SOUND_FEATURE 0 // 0 = Sound decoder disabled, 1 = Sound decoder enabled
 #define CAR_DECODER 1   // 0 = Car decoder disabled, 1 = Car decoder enabled
@@ -17,15 +11,6 @@
 // Uncomment to force CV Reset to Factory Defaults
 //#define FactoryDefaults
 //#define EEPROM_Clear
-
-// Uncomment any of the lines below to enable debug messages for different parts of the code
-//#define DEBUG_FUNCTIONS
-//#define DEBUG_FUNC
-//#define DEBUG_SPEED
-//#define DEBUG_PWM
-//#define DEBUG_DCC_ACK
-//#define DEBUG_DCC_MSG
-//#define DEBUG_PRINT
 
 #endif
 
