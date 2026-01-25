f92aa8914b946a9f85013e6df1cea48fbd1ffce4
Andy <azogg65@bluewin.ch>
2026-01-25 16:22:15 +0100

    Add: Create PinConfig.h for pin configuration definitions

---
+++ src/PinConfig.h
@@ -0,0 +1,17 @@
+// PinConfig.h
+
+#ifndef _PINCONFIG_h
+#define _PINCONFIG_h
+
+#define DCC_PIN         2 
+#define DCC_PIN_INT     PCINT2   // Interrupt number of the pin
+
+#define LED_PIN_FWD     0   // AUX 1
+#define LED_PIN_REV     1   // AUX 2
+#define DCC_ACK_PIN     LED_PIN_FWD
+
+#define NUM_OUTPUTS 2
+
+
+#endif
+
